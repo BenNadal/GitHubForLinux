d3a4e877b1894498aef3c8c67e284c62b3fa8f34
BenNadal <55394701+BenNadal@users.noreply.github.com>
2019-09-24 09:16:53 +0200

    Add files via upload
    site du restaurant 2.0

---
+++ resto/Html/cible.php
@@ -0,0 +1,7 @@
+<p>Bonjour !</p>
+
+<p>Tu t'appelles <?php echo $_POST['prenom']; ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="livreor.html">clique ici</a> 
+pour revenir à la page formulaire.php.
+</p>
